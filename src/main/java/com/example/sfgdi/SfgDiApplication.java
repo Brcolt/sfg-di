37a6fa02f300a4d4a65fda49c26626324c041a26
Bruno Colt <brunocolt.ufrn@gmail.com>
2020-10-27 16:04:56 -0300

    Primary Bean e Qualifier
    Testando os conceitos de Primary Bean e Qualifiers

--- src/main/java/com/example/sfgdi/SfgDiApplication.java
+++ src/main/java/com/example/sfgdi/SfgDiApplication.java
@@ -12,31 +12,23 @@ import org.springframework.context.ApplicationContext;
 public class SfgDiApplication {
 
 	public static void main(String[] args) {
-
 		ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
 
 		MyController myController = (MyController) ctx.getBean("myController");
 
-		String greeting = myController.sayHello();
-
-		System.out.println(greeting);
+		System.out.println("Primary Bean");
+		System.out.println(myController.sayHello());
 
 		System.out.println("---------Property");
-
 		PropertyInjectedController propertyInjectedController = (PropertyInjectedController) ctx.getBean("propertyInjectedController");
-
 		System.out.println(propertyInjectedController.getGreeting());
 
-		System.out.println("---------SetInjection - Controller");
-
+		System.out.println("---------SetInjection");
 		SetterInjectedController setterInjectedController = (SetterInjectedController) ctx.getBean("setterInjectedController");
-
 		System.out.println(setterInjectedController.getGreeting());
 
 		System.out.println("---------Constructor");
-
 		ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController) ctx.getBean("constructorInjectedController");
-
 		System.out.println(constructorInjectedController.getGreeting());
 	}
 
--- src/main/java/com/example/sfgdi/controllers/PropertyInjectedController.java
+++ src/main/java/com/example/sfgdi/controllers/PropertyInjectedController.java
@@ -2,12 +2,14 @@ package com.example.sfgdi.controllers;
 
 import com.example.sfgdi.services.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 
 @Controller
 public class PropertyInjectedController {
 
+    @Qualifier("propertyInjectionGreetingService")
     @Autowired
     public GreetingService greetingService;
 
